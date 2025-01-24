4e362bb8a5c84642e10e8a32908c866687b8a1f4
ChrisEberleSchool <156865868+ChrisEberleSchool@users.noreply.github.com>
2025-01-24 15:21:14 -0700

    Upload Project template
    Maven project with a basic JavaFX launcher window.

---
+++ stockfolio/src/main/java/com/chriseberle/App.java
@@ -0,0 +1,15 @@
+package com.chriseberle;
+
+import com.chriseberle.applications.LauncherApp;
+import javafx.application.Application;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        Application.launch(LauncherApp.class, args);
+        System.out.println("Hello World");
+    }
+
+}
+
